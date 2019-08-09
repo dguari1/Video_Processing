ae52b825234e011980f6d1b83e223dbe6541075b
Diego L. Guarin <diego.guarinlopez@mail.mcgill.ca>
2019-08-09 15:56:58 -0400

    small changes

--- processing_window.py
+++ processing_window.py
@@ -25,6 +25,8 @@ class ProcessingWindow(QtWidgets.QWidget):
         #self.setWindowIcon(QtGui.QIcon(scriptDir + os.path.sep + 'include' +os.path.sep +'icon_color'+ os.path.sep + 'ruler_icon.ico'))
 
 
+        self.numberoflistboundingbox = 1 #this variable counts the number of list used to crete the frames in the bounding box 
+
         # initialize the User Interface
         self.initUI()
         self.show()
@@ -84,9 +86,41 @@ class ProcessingWindow(QtWidgets.QWidget):
         label_firstframes.setFont(newfont)
         label_firstframes.setFixedWidth(100)
 
-        label_frames = QtWidgets.QLabel('Frames')
-        label_frames.setFont(newfont)
-        label_frames.setFixedWidth(100)
+        label_frames1 = QtWidgets.QLabel('Frames')
+        label_frames1.setFont(newfont)
+        label_frames1.setFixedWidth(100)
+        
+        label_frames2 = QtWidgets.QLabel('Frames')
+        label_frames2.setFont(newfont)
+        label_frames2.setFixedWidth(100)
+        
+        label_init = QtWidgets.QLabel('init')
+        label_init.setFont(QtGui.QFont("Times", 10))
+        label_init.setMinimumWidth(25)
+        label_init.setAlignment(QtCore.Qt.AlignCenter)
+        
+        label_step = QtWidgets.QLabel('step')
+        label_step.setFont(QtGui.QFont("Times", 10))
+        label_step.setMinimumWidth(25)
+        label_step.setAlignment(QtCore.Qt.AlignCenter)
+        
+        label_end = QtWidgets.QLabel('end')
+        label_end.setFont(QtGui.QFont("Times", 10))
+        label_end.setMinimumWidth(25)
+        label_end.setAlignment(QtCore.Qt.AlignCenter)
+        
+        label_empty = QtWidgets.QLabel('')
+        label_empty.setFont(QtGui.QFont("Times", 10))
+        label_empty.setFixedWidth(25)
+        label_empty.setFixedHeight(25)
+        label_empty.setAlignment(QtCore.Qt.AlignCenter)
+        
+        layoutadditionalframes_labels = QtWidgets.QHBoxLayout()
+        layoutadditionalframes_labels.addWidget(label_init, QtCore.Qt.AlignCenter)
+        layoutadditionalframes_labels.addWidget(label_step, QtCore.Qt.AlignCenter)
+        layoutadditionalframes_labels.addWidget(label_end, QtCore.Qt.AlignCenter)
+        layoutadditionalframes_labels.addWidget(label_empty)
+        
 
         self.allframestick_boundingbox = QtWidgets.QCheckBox('')
         self.allframestick_boundingbox.setChecked(False)
@@ -102,17 +136,54 @@ class ProcessingWindow(QtWidgets.QWidget):
         self.framelistboundingbox.setFixedHeight(25)
         self.framelistboundingbox.setFont(QtGui.QFont("Times", 8))
 
-        regex = QtCore.QRegExp("^(\d+(,\d+)*)?$")
-        regval = QtGui.QRegExpValidator(regex)
-        self.framelistboundingbox.setValidator(regval)
+        self.framelistboundingbox.setValidator(QtGui.QRegExpValidator(QtCore.QRegExp("^(\d+(,\d+)*)?$")))
+        
+        RegExpVal = QtGui.QRegExpValidator(QtCore.QRegExp("^(\d+)?$"))
+        
+        self.frameinitboundingbox_1 = QtWidgets.QLineEdit()
+        self.frameinitboundingbox_1.setMinimumWidth(50)
+        self.frameinitboundingbox_1.setFixedHeight(25)
+        self.frameinitboundingbox_1.setFont(QtGui.QFont("Times", 8))
+        self.frameinitboundingbox_1.setValidator(RegExpVal)
+        self.frameinitboundingbox_1.setAlignment(QtCore.Qt.AlignCenter)
+        
+        self.framestepboundingbox_1 = QtWidgets.QLineEdit()
+        self.framestepboundingbox_1.setMinimumWidth(50)
+        self.framestepboundingbox_1.setFixedHeight(25)
+        self.framestepboundingbox_1.setFont(QtGui.QFont("Times", 8))
+        self.framestepboundingbox_1.setValidator(RegExpVal)
+        self.framestepboundingbox_1.setAlignment(QtCore.Qt.AlignCenter)
+        
+        self.frameendboundingbox_1 = QtWidgets.QLineEdit()
+        self.frameendboundingbox_1.setMinimumWidth(50)
+        self.frameendboundingbox_1.setFixedHeight(25)
+        self.frameendboundingbox_1.setFont(QtGui.QFont("Times", 8))
+        self.frameendboundingbox_1.setValidator(RegExpVal)
+        self.frameendboundingbox_1.setAlignment(QtCore.Qt.AlignCenter)
+        
+        self.buttontoaddlines = QtWidgets.QPushButton('')
+        self.buttontoaddlines.setIcon(QtGui.QIcon('./icons/plus.png'))
+        self.buttontoaddlines.setIconSize(QtCore.QSize(25,25))
+        
+        
+        
+        layoutadditionalframes_lines = QtWidgets.QHBoxLayout()
+        layoutadditionalframes_lines.addWidget(self.frameinitboundingbox_1)
+        layoutadditionalframes_lines.addWidget(self.framestepboundingbox_1)
+        layoutadditionalframes_lines.addWidget(self.frameendboundingbox_1)
+        layoutadditionalframes_lines.addWidget(self.buttontoaddlines)
+        
 
         self.boundingboxLayout.addWidget(label_allframes, 0, 0, 1, 1)
         self.boundingboxLayout.addWidget(self.allframestick_boundingbox, 0, 1, 1, 1)
         self.boundingboxLayout.addWidget(label_firstframes, 1, 0, 1, 1)
         self.boundingboxLayout.addWidget(self.firstframetick_boundingbox, 1, 1, 1, 1)
-        self.boundingboxLayout.addWidget(label_frames, 2, 0, 1, 1)
+        self.boundingboxLayout.addWidget(label_frames1, 2, 0, 1, 1)
         self.boundingboxLayout.addWidget(self.framelistboundingbox, 2, 1, 1, 1)
-
+        
+        self.boundingboxLayout.addLayout(layoutadditionalframes_labels, 3, 1, 1, 1)
+        self.boundingboxLayout.addWidget(label_frames2, 4, 0, 1, 1)
+        self.boundingboxLayout.addLayout(layoutadditionalframes_lines, 4, 1, 1, 1)
 
 
         self.setLayout(layout)
@@ -151,7 +222,18 @@ class Test(QtWidgets.QWidget):
 
         
 if __name__ == '__main__':
-    app = QtWidgets.QApplication([])
+#    app = QtWidgets.QApplication([])
+#    GUI = ProcessingWindow()
+#    GUI.show()
+#    app.exec_()
+#    
+    if not QtWidgets.QApplication.instance():
+        app = QtWidgets.QApplication(sys.argv)
+    else:
+        app = QtWidgets.QApplication.instance()
+
+    app.setStyle(QtWidgets.QStyleFactory.create('Cleanlooks'))
+
     GUI = ProcessingWindow()
     GUI.show()
     app.exec_()
